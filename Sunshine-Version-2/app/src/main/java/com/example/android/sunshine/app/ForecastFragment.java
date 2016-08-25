52d27aac1a38a9d5d7e3b4128558c052113462d9
Володя <>
2016-08-25 06:53:54 +0300

    Добавлен полный код парсинга, а также обновление ArrayAdapter

--- Sunshine-Version-2/app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ Sunshine-Version-2/app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -25,6 +26,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -33,6 +35,8 @@ import java.util.List;
  * Created by Володя on 13.08.2016.
  */
 public class ForecastFragment extends Fragment {
+
+    private ArrayAdapter<String> mForecastAdapter;
 
     public ForecastFragment() {
     }
@@ -83,7 +87,7 @@ public class ForecastFragment extends Fragment {
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
 
         // context Текущий контекст
-        ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(getActivity(),
+        mForecastAdapter = new ArrayAdapter<String>(getActivity(),
                 // resource ID ресурса layout-файла, содержащего макет для использования при создании экземпляров View
                 R.layout.list_item_forecast,
                 // textViewResourceId ID ресурса TextView, который должен быть заполнен, в ресурсе layout-файла
@@ -95,18 +99,116 @@ public class ForecastFragment extends Fragment {
         listView.setAdapter(mForecastAdapter);
 
         new FetchWeatherTask().execute("94043");
-//        new FetchWeatherTask().execute();
 
         return rootView;
     }
 
-    public class FetchWeatherTask extends AsyncTask<String, Void, String>
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]>
     {
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
+        /* The date/time conversion code is going to be moved outside the asynctask later,
+         * so for convenience we're breaking it out into its own method now.
+         */
+        private String getReadableDateString(long time){
+            // Because the API returns a unix timestamp (measured in seconds),
+            // it must be converted to milliseconds in order to be converted to valid date.
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return shortenedDateFormat.format(time);
+        }
+
+        /**
+         * Prepare the weather high/lows for presentation.
+         */
+        private String formatHighLows(double high, double low) {
+            // For presentation, assume the user doesn't care about tenths of a degree.
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            String highLowStr = roundedHigh + "/" + roundedLow;
+            return highLowStr;
+        }
+
+        /**
+         * Take the String representing the complete forecast in JSON Format and
+         * pull out the data we need to construct the Strings needed for the wireframes.
+         *
+         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
+         * into an Object hierarchy for us.
+         */
+        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DESCRIPTION = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+            // OWM returns daily forecasts based upon the local time of the city that is being
+            // asked for, which means that we need to know the GMT offset to translate this data
+            // properly.
+
+            // Since this data is also sent in-order and the first day is always the
+            // current day, we're going to take advantage of that to get a nice
+            // normalized UTC date for all of our weather.
+
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            // we start at the day returned by local time. Otherwise this is a mess.
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+            // now we work exclusively in UTC
+            dayTime = new Time();
+
+            String[] resultStrs = new String[numDays];
+            for(int i = 0; i < weatherArray.length(); i++) {
+                // For now, using the format "Day, description, hi/low"
+                String day;
+                String description;
+                String highAndLow;
+
+                // Get the JSON object representing the day
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                // The date/time is returned as a long.  We need to convert that
+                // into something human-readable, since most people won't read "1400356800" as
+                // "this saturday".
+                long dateTime;
+                // Cheating to convert this to UTC time, which is what we want anyhow
+                dateTime = dayTime.setJulianDay(julianStartDay+i);
+                day = getReadableDateString(dateTime);
+
+                // description is in a child array called "weather", which is 1 element long.
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+
+                // Temperatures are in a child object called "temp".  Try not to name variables
+                // "temp" when working with temperature.  It confuses everybody.
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.getDouble(OWM_MAX);
+                double low = temperatureObject.getDouble(OWM_MIN);
+
+                highAndLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+            }
+
+            for (String s : resultStrs) {
+                Log.v(LOG_TAG, "Forecast entry: " + s);
+            }
+            return resultStrs;
+
+        }
+
         @Override
-        protected String doInBackground(String... params) {
+        protected String[] doInBackground(String... params) {
 
             // Если нет почтового индекса, нечего искать. Проверка размера массива параметров
             if (params.length == 0) {
@@ -134,7 +236,6 @@ public class ForecastFragment extends Fragment {
                 final String DAYS_PARAM = "cnt";
                 final String APPID_PARAM = "APPID";
 
-                // TODO: Забыла добавить APPKEY
                 // Метод parse создаёт Uri, которое парсит данную закодированную Uri-строку
                 // Метод buildUpon создает новый builder, копируя атрибуты из этого Uri
                 Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
@@ -198,47 +299,28 @@ public class ForecastFragment extends Fragment {
                     }
                 }
             }
-            //return null;
-            return forecastJsonStr;
+
+            try {
+                return getWeatherDataFromJson(forecastJsonStr, numDays);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+            return null;
         }
 
         @Override
-        protected void onPostExecute(String forecastJsonStr) {
-            super.onPostExecute(forecastJsonStr);
+        protected void onPostExecute(String[] strings) {
+            super.onPostExecute(strings);
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(strings));
+            mForecastAdapter.clear();
 
-            //getMaxTemperatureForDay(forecastJsonStr, 0);
-            JSONObject dataJsonObj = null;
-            String maxTemp = "";
-
-            try {
-                getMaxTemperatureForDay(forecastJsonStr, 0);
-            } catch (JSONException e){
-                e.printStackTrace();
+            if (weekForecast != null){
+                for (String string : weekForecast){
+                    mForecastAdapter.add(string);
+                }
             }
-        }
-
-        private double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
-        throws JSONException {
-            // Создаём новый JSONObject
-            JSONObject jsonObject = new JSONObject(weatherJsonStr);
-
-            // Получаем массив JSONArray по имени. В данном случае нас интересует массив list
-            JSONArray list = jsonObject.getJSONArray("list");
-
-            // Получаем из массива информацию о необходимом дне по индексу
-            JSONObject day = list.getJSONObject(dayIndex);
-
-            // Из найденного объекта day получаем информацию о температуре - объект temp
-            JSONObject temp = day.getJSONObject("temp");
-
-            // Из найденного объекта temp получаем вещественное число, содержащее максимальную температуру //TODO: Неизвестно, откуда она берётся. Если просто выполнить запрос по строке, там совершенно другие температуры
-            double maxTemp = temp.getDouble("max");
-
-            // Выводим результат в лог
-            Log.d(LOG_TAG, "Максимальная температура: " + maxTemp);
-
-            // Возвращаем результат
-            return maxTemp;
+            mForecastAdapter.notifyDataSetChanged();
         }
     }
 }
