16c850bebaca485d317bda9590970a12d565b05c
Володя <>
2016-11-08 06:42:25 +0300

    Добавлено построение URI с погодой и местоположением

--- Sunshine-Version-2/app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ Sunshine-Version-2/app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -15,6 +15,9 @@
  */
 package com.example.android.sunshine.app.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 import android.text.format.Time;
 
@@ -25,21 +28,21 @@ public class WeatherContract {
 
     //------------------------------------------------------------------------------------------
     // Добавлено из гиста
-    // The "Content authority" is a name for the entire content provider, similar to the
-    // relationship between a domain name and its website.  A convenient string to use for the
-    // content authority is the package name for the app, which is guaranteed to be unique on the
-    // device.
+    // "Content authority" - это имя целого контент-провайдера, аналогичное
+    // отношению между доменным именем и его веб-сайтом. Удобная строка для использования
+    // авторитетного источника контента - имя пакета приложения, что гарантирует уникальность на
+    // устройстве.
     public static final String CONTENT_AUTHORITY = "com.example.android.sunshine.app";
 
-    // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
-    // the content provider.
+    // Используем CONTENT_AUTHORITY, чтобы создать базовую часть для всех URI, которые приложение
+    // будет использовать, чтобы контактировать с контент-провайдером.
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
-    // Possible paths (appended to base content URI for possible URI's)
-    // For instance, content://com.example.android.sunshine.app/weather/ is a valid path for
-    // looking at weather data. content://com.example.android.sunshine.app/givemeroot/ will fail,
-    // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
-    // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
+    // Возможные пути (добавляемые к базовому контентному URI для возможных URI)
+    // Например, content://com.example.android.sunshine.app/weather/ - допустимый путь для
+    // просмотра погодных данных. content://com.example.android.sunshine.app/givemeroot/ не сработает,
+    // поскольку ContentProvider не имеет информации, что делать с "givemeroot".
+    // По крайней мере, будем надеяться. Don't be that dev, reader.  Don't be that dev.
     public static final String PATH_WEATHER = "weather";
     public static final String PATH_LOCATION = "location";
     //------------------------------------------------------------------------------------------
@@ -62,9 +65,17 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         //------------------------------------------------------------------------------------------
         // Добавлено из гиста
+        // Представляет базовое местоположение таблицы
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
 
+        // Курсоры, возвращаемые из контент-провайдера, имеют уникальные типы, основанные на их
+        // контенте и базовом пути, использованном для запроса. Андроид использует форму, аналогичную
+        // internet media type, или MIME type, чтобы описать тип, возвращаемый URI.
+        // Курсоры, которые могут содержать больше одного элемента, содержат в качестве префикса
+        // строку CURSOR_DIR_BASE_TYPE, тогда как курсоры, которые возвращают только один элемент,
+        // содержат в качестве префикса строку CURSOR_ITEM_BASE_TYPE. Эти типы определены как для
+        // запроса местоположения, так и для запроса погоды.
         public static final String CONTENT_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
         public static final String CONTENT_ITEM_TYPE =
@@ -72,26 +83,29 @@ public class WeatherContract {
 
         public static Uri buildLocationUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
-        //------------------------------------------------------------------------------------------
+        }
+            //------------------------------------------------------------------------------------------
 
-        public static final String TABLE_NAME = "location";
+            public static final String TABLE_NAME = "location";
 
-        // Строка с настройкой местоположения - это то, что будет передано в openweathermap как запрос местоположения
-        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+            // Строка с настройкой местоположения - это то, что будет передано в openweathermap как запрос местоположения
+            public static final String COLUMN_LOCATION_SETTING = "location_setting";
 
-        // Читаемая строка местоположения, предоставляемая API
-        public static final String COLUMN_CITY_NAME = "city_name";
+            // Читаемая строка местоположения, предоставляемая API
+            public static final String COLUMN_CITY_NAME = "city_name";
 
-        // Чтобы уникально указать местоположение на карте при использовании map-интента,
-        // мы храним долготу и широту, как нам вернул их openweathermap API.
-        public static final String COLUMN_COORD_LAT = "coord_lat";
-        public static final String COLUMN_COORD_LONG = "coord_long";
+            // Чтобы уникально указать местоположение на карте при использовании map-интента,
+            // мы храним долготу и широту, как нам вернул их openweathermap API.
+            public static final String COLUMN_COORD_LAT = "coord_lat";
+            public static final String COLUMN_COORD_LONG = "coord_long";
+
     }
 
     /* Inner class that defines the table contents of the weather table */
     public static final class WeatherEntry implements BaseColumns {
         //------------------------------------------------------------------------------------------
         // Добавлено из гиста
+        // Представляет базовое местоположение таблицы
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
 
@@ -99,46 +113,6 @@ public class WeatherContract {
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
-
-
-        public static Uri buildWeatherUri(long id) {
-            return ContentUris.withAppendedId(CONTENT_URI, id);
-        }
-
-        /*
-            Student: Fill in this buildWeatherLocation function
-         */
-        public static Uri buildWeatherLocation(String locationSetting) {
-            return null;
-        }
-
-        public static Uri buildWeatherLocationWithStartDate(
-                String locationSetting, long startDate) {
-            long normalizedDate = normalizeDate(startDate);
-            return CONTENT_URI.buildUpon().appendPath(locationSetting)
-                    .appendQueryParameter(COLUMN_DATE, Long.toString(normalizedDate)).build();
-        }
-
-        public static Uri buildWeatherLocationWithDate(String locationSetting, long date) {
-            return CONTENT_URI.buildUpon().appendPath(locationSetting)
-                    .appendPath(Long.toString(normalizeDate(date))).build();
-        }
-
-        public static String getLocationSettingFromUri(Uri uri) {
-            return uri.getPathSegments().get(1);
-        }
-
-        public static long getDateFromUri(Uri uri) {
-            return Long.parseLong(uri.getPathSegments().get(2));
-        }
-
-        public static long getStartDateFromUri(Uri uri) {
-            String dateString = uri.getQueryParameter(COLUMN_DATE);
-            if (null != dateString && dateString.length() > 0)
-                return Long.parseLong(dateString);
-            else
-                return 0;
-        }
         //------------------------------------------------------------------------------------------
 
         public static final String TABLE_NAME = "weather";
@@ -170,5 +144,61 @@ public class WeatherContract {
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
 
+        //------------------------------------------------------------------------------------------
+        // Добавлено из гиста
+        // Функции, которые помогают строить контент-провайдерные запросы.
+        // Их удобно именть, потому что это занимает меньше места в вашем коде, знающем
+        // о фактическом кодировании URI, сохраняя эти знания в контракте.
+        public static Uri buildWeatherUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        /*
+            Student: Fill in this buildWeatherLocation function
+         */
+        public static Uri buildWeatherLocation(String locationSetting) {
+            Uri weatherLocationUri = CONTENT_URI.buildUpon()
+                    .appendPath(locationSetting)
+                    .build();
+            return weatherLocationUri;
+        }
+
+        // Также мы можем использовать эти функции, чтобы добавлять потенциадьно полезные параметры
+        // запроса. В данном случае мы используем параметр запроса для даты начала. Параметры запроса
+        // полезны, когда мы имеем фиксированный запрос к базе данных, для которого мы можем захотеть
+        // иметь некоторую ограниченную параметризацию. В данном случае это будет параметр для связи
+        // между двумя таблицами.
+        public static Uri buildWeatherLocationWithStartDate(
+                String locationSetting, long startDate) {
+            long normalizedDate = normalizeDate(startDate);
+            // Строки, такие как locationSetting, могут быть добавлены с помощью функции appendPath
+            return CONTENT_URI.buildUpon().appendPath(locationSetting)
+                    .appendQueryParameter(COLUMN_DATE, Long.toString(normalizedDate)).build();
+        }
+
+        // Эта функция строит двухчастный URI как с сегментом погоды, так и с сегментом даты.
+        public static Uri buildWeatherLocationWithDate(String locationSetting, long date) {
+            return CONTENT_URI.buildUpon().appendPath(locationSetting)
+                    .appendPath(Long.toString(normalizeDate(date))).build();
+        }
+
+        // Вспомогательные функции, которые прячут структуру URI от кода
+        // для возвращения значений в URI и помещения этих знаний в одно место
+        public static String getLocationSettingFromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        public static long getDateFromUri(Uri uri) {
+            return Long.parseLong(uri.getPathSegments().get(2));
+        }
+
+        public static long getStartDateFromUri(Uri uri) {
+            String dateString = uri.getQueryParameter(COLUMN_DATE);
+            if (null != dateString && dateString.length() > 0)
+                return Long.parseLong(dateString);
+            else
+                return 0;
+        }
+        //------------------------------------------------------------------------------------------
     }
 }
